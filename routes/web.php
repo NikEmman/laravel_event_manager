3be256a6a1617e6d46529845d2ef48ae85e64709
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-01-27 18:58:34 +0200

    Group routes together for readability

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/login', function () {
-    return view('login');
-});
+// Register routes
 Route::get('/register', function () {
     return view('register');
 });
 Route::post('/register',[UserController::class,'register']);
 Route::post('/logout',[UserController::class,'logout']);
+
+// Login routes
+Route::get('/login', function () {
+    return view('login');
+});
 Route::post('/login',[UserController::class,'login']);
+
