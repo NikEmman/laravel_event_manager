04cdac7bbd2070e0178c29a843d2159445a51e95
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-01-27 22:17:01 +0200

    Add register link to login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,10 @@
-@extends('app') @section('title', 'Log In') @section('content') 
-<h2>Login</h2>
+@extends('app') @section('title', 'Log In') @section('content')
+    <h2>Login</h2>
     <form action="/login" method="POST">
         @csrf
         <input name="name" type="text" placeholder="Username">
         <input name="password" type="password" placeholder="Password">
         <button>Log In</button>
     </form>
+    <p>Don't have an account?<a href="/register"> Register for free!</a></p>
 @endsection
