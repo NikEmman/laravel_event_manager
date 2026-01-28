b19f2e034fb190ea24c3ac7acd7fa08a10fa384a
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-01-28 21:48:13 +0200

    Update UserController login action
    Login action now requires and expects email instead of name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,44 +7,49 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
-{   
-    public function logout(){
-    auth()->logout();
-    return redirect("/");
-}
-   public function register(Request $request)
 {
-    // 1. Validation - If this fails, Laravel handles the redirect for you automatically.
-    $incomingFields = $request->validate([
-        "name"=> ["required", "min:3", Rule::unique('users','name')],
-        "email"=> ["required", "email", Rule::unique('users','email')],
-        "password"=> ["required", "min:8"]
-    ]);
-
-    // 2. Prepare data & create user
-    $incomingFields["password"] = bcrypt($incomingFields["password"]);
-    $user = User::create($incomingFields);
-
-    // 3. Log the user in
-    auth()->login($user);
-   
-    // 4. Redirect with success message
-    return redirect('/')->with('success', 'Congrats! Your account is ready.');
-}
-    public function login(Request $request) {
-    $incommingFields = $request->validate([
-        'name' => 'required',
-        'password' => 'required'
-    ]);
-
-    if (auth()->attempt([
-        'name' => $incommingFields['name'], 
-        'password' => $incommingFields['password']
-    ])) {
-        $request->session()->regenerate();
-        return redirect('/')->with('success', 'Logged in successfully!');
+    public function logout()
+    {
+        auth()->logout();
+        return redirect("/");
     }
+    public function register(Request $request)
+    {
+        // 1. Validation - If this fails, Laravel handles the redirect for you automatically.
+        $incomingFields = $request->validate([
+            "name" => ["required", "min:3", Rule::unique('users', 'name')],
+            "email" => ["required", "email", Rule::unique('users', 'email')],
+            "password" => ["required", "min:8"]
+        ]);
 
-    // This triggers if the name/password don't match the DB
-    return redirect('/login')->with('failure', 'Invalid username or password.');}
+        // 2. Prepare data & create user
+        $incomingFields["password"] = bcrypt($incomingFields["password"]);
+        $user = User::create($incomingFields);
+
+        // 3. Log the user in
+        auth()->login($user);
+
+        // 4. Redirect with success message
+        return redirect('/')->with('success', 'Congrats! Your account is ready.');
+    }
+    public function login(Request $request)
+    {
+        $incommingFields = $request->validate([
+            'email' => ['required', "email"],
+            'password' => 'required'
+        ]);
+
+        if (
+            auth()->attempt([
+                'email' => $incommingFields['email'],
+                'password' => $incommingFields['password']
+            ])
+        ) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'Logged in successfully!');
+        }
+
+        // This triggers if the name/password don't match the DB
+        return redirect('/login')->with('failure', 'Invalid username or password.');
+    }
 }
